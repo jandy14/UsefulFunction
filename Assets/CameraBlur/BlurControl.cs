38b3c1b842f0ef46ec58287116184b5cf170076f
jandy14 <seonguk14@changwon.ac.kr>
2020-02-27 16:11:39 +0900

    눈 뜨는 효과 적용

--- Assets/CameraBlur/BlurControl.cs
+++ Assets/CameraBlur/BlurControl.cs
@@ -8,8 +8,12 @@ public class BlurControl : MonoBehaviour
 {
 	public PostProcessVolume ppv;
 	public Image img;
+	public Image up;
+	public Image down;
 	public AnimationCurve curve;
+	public AnimationCurve eyeCurve;
 	public float time;
+	public float eyeTime;
 
 	// Start is called before the first frame update
 	void Start()
@@ -27,12 +31,17 @@ public class BlurControl : MonoBehaviour
 	{
 		yield return new WaitForSeconds(1f);
 		float timer = 0;
-
+		Vector3 dest = Vector3.up * 1500;
+		Vector3 start = up.rectTransform.localPosition;
+		Debug.Log(start);
 		while(timer < time)
 		{
-			Debug.Log(Mathf.Lerp(5, 0, curve.Evaluate(timer / time)));
 			img.material.SetFloat("_Radius", Mathf.Lerp(15, 0, curve.Evaluate(timer / time)));
 			//ppv.weight = Mathf.Lerp(1, 0.6f, curve.Evaluate(timer / time));
+
+			up.rectTransform.localPosition = Vector3.Lerp(start, dest, eyeCurve.Evaluate(timer / eyeTime));
+			down.rectTransform.localPosition = Vector3.Lerp(-start, -dest, eyeCurve.Evaluate(timer / eyeTime));
+
 			timer += Time.deltaTime;
 			yield return null;
 		}
