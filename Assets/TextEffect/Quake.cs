c97b0873a9c1e92c5ef5b3a6f1090f8a64fc66c4
jandy14 <seonguk14@changwon.ac.kr>
2020-02-28 23:41:46 +0900

    글자 시작 충격 추가

---
+++ Assets/TextEffect/Quake.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quake : MonoBehaviour
+{
+	public float power;
+	public float torque;
+    // Start is called before the first frame update
+    void Start()
+    {
+		GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(-1f,power), Random.Range(-1f,power)));
+		GetComponent<Rigidbody2D>().AddTorque( Random.Range(-torque, torque) );
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
