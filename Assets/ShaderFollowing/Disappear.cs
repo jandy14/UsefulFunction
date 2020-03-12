21038d76c82c61931cfc0231992a2fb49c74e696
jandy14 <seonguk14@changwon.ac.kr>
2020-03-12 19:09:43 +0900

    disappear 쉐이더 자동으로 값 조정 스크립트, 복잡한 3D모델에도 적용 실험

---
+++ Assets/ShaderFollowing/Disappear.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disappear : MonoBehaviour
+{
+	public Renderer rd;
+	private void Start()
+	{
+		rd = GetComponent<Renderer>();
+	}
+	// Update is called once per frame
+	void Update()
+    {
+		rd.material.SetFloat("_Threshold", Mathf.Sin(Time.time) * 0.5f + 0.5f );
+    }
+}
