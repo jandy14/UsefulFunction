bbf479f65581bc97e04a387dbd8eb7973b5631df
jandy14 <seonguk14@changwon.ac.kr>
2020-03-21 15:21:40 +0900

    pixel edge filter 를 카메라에 적용
    post processing의 bloom과 같이 쓰니 이전 화면의 데이터가 남는 문제가 있어 post processing은 꺼줌

---
+++ Assets/PostProcessingEffect/PostProcessing_Profiles/PixelEdgeFilter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelEdgeFilter : MonoBehaviour
+{
+	private Material material;
+
+	public Color BaseColor;
+	public Color EdgeColor;
+	public float Threshold;
+
+	void Awake()
+	{
+		material = new Material(Shader.Find("Unlit/PixelFilter"));
+	}
+	void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		material.SetColor("_BaseColor", BaseColor);
+		material.SetColor("_OutlineColor", EdgeColor);
+		material.SetFloat("_Threshold", Threshold);
+		Graphics.Blit(source, destination, material);
+	}
+}
