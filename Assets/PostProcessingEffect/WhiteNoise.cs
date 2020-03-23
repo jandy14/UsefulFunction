6345f28b634b5d633cfe0c4835fbe144d26dd6d9
jandy14 <seonguk14@changwon.ac.kr>
2020-03-23 13:37:09 +0900

    white noise effect

---
+++ Assets/PostProcessingEffect/WhiteNoise.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhiteNoise : MonoBehaviour
+{
+	private Material material;
+	[Range(0f,1f)]
+	public float strength = 0.1f;
+	// Creates a private material used to the effect
+	void Awake()
+	{
+		material = new Material(Shader.Find("Hidden/WhiteNoise"));
+	}
+
+	// Postprocess the image
+	void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		material.SetFloat("_Strength", strength);
+		Graphics.Blit(source, destination, material);
+	}
+}
