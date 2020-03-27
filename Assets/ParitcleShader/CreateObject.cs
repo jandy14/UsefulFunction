613e17d2f3a24aa3d5a04bf2c08132d1240efe36
jandy14 <seonguk14@changwon.ac.kr>
2020-03-27 19:30:40 +0900

    스크립트로 구현한 avoiding cupe

--- Assets/ParitcleShader/CreateObject.cs
+++ Assets/ParitcleShader/CreateObject.cs
@@ -9,12 +9,17 @@ public class CreateObject : MonoBehaviour
 	public Vector3 startPos;
 	public Vector3 endPos;
 	public float dist;
+	public bool isShader = true;
 
+	private MoveCube[] moveCubes;
 	private Material[] particles;
 	void Awake()
     {
 		int amount = ((int)(Mathf.Abs(endPos.x - startPos.x)/dist) + 1) * ((int)(Mathf.Abs(endPos.z - startPos.z)/dist) + 1);
-		particles = new Material[amount];
+		if(isShader)
+			particles = new Material[amount];
+		else
+			moveCubes = new MoveCube[amount];
 		MakeObject();
     }
 
@@ -26,7 +31,10 @@ public class CreateObject : MonoBehaviour
 			for (float z = startPos.z; z < endPos.z; z += dist)
 			{
 				GameObject tmp = Instantiate(obj, new Vector3(x, startPos.y, z), Quaternion.identity);
-				particles[i++] = tmp.GetComponent<Renderer>().material;
+				if (isShader)
+					particles[i++] = tmp.GetComponent<Renderer>().material;
+				else
+					moveCubes[i++] = tmp.GetComponent<MoveCube>();
 			}
 		}
 	}
@@ -40,10 +48,21 @@ public class CreateObject : MonoBehaviour
 				particles[i].SetVector("_Position", pos);
 		}
 	}
-
+	void SetScriptPosition()
+	{
+		Vector3 pos = player.transform.position;
+		for (int i = 0; i < moveCubes.Length; ++i)
+		{
+			if (moveCubes[i] != null)
+				moveCubes[i].Position = pos;
+		}
+	}
     // Update is called once per frame
     void Update()
     {
-		SetPosition();
+		if (isShader)
+			SetPosition();
+		else
+			SetScriptPosition();
     }
 }
---
+++ Assets/ParitcleShader/PerformanceTest/MoveCube.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour
+{
+	
+	public Vector3 Position;
+	public float Range = 5;
+
+	private Vector3 pivot;
+
+	private void Start()
+	{
+		pivot = transform.position;
+	}
+
+	void SetPosition()
+	{
+		float dist = Vector3.Distance(pivot, Position);
+		Vector3 move = Vector3.zero;
+		if(dist < Range)
+			move = Vector3.Normalize(pivot - Position) * (Range - dist);
+		transform.position = pivot + move;
+	}
+    void Update()
+    {
+		SetPosition();
+    }
+}
