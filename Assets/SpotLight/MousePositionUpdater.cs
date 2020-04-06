47243c743586cc2f2ab0e7ca05d0fa13a9c63398
jandy14 <seonguk14@changwon.ac.kr>
2020-04-06 15:42:21 +0900

    3DSpotlight shader
    마우스 위치는 raycast를 이용

---
+++ Assets/SpotLight/MousePositionUpdater.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePositionUpdater : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hitData;
+
+		if (Physics.Raycast(ray, out hitData, 1000))
+		{
+			//Debug.Log(hitData.point);
+			Shader.SetGlobalVector("_MousePosition", hitData.point);
+		}
+	}
+}
