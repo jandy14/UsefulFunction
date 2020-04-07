815a3aae9d13539ebb368ec7743e56eee952788b
jandy14 <seonguk14@changwon.ac.kr>
2020-04-07 12:44:36 +0900

    i don't know what it is

---
+++ Assets/NoTitle/DefaultImageEffect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultImageEffect : MonoBehaviour
+{
+	public Shader shader;
+
+	[SerializeField]
+	private Material mat;
+
+	void Awake()
+	{
+		mat = new Material(shader);
+	}
+
+	void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		Graphics.Blit(source, destination, mat);
+	}
+}
