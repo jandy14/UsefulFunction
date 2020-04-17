f4cd474d3f7cce02bbe89522926df9601e6ef868
jandy14 <seonguk14@changwon.ac.kr>
2020-04-17 15:24:53 +0900

    텍스처를 이용한 장면 전환 효과

---
+++ Assets/SceneSwitching/SwitchScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchScene : MonoBehaviour
+{
+	private Material material;
+	public Texture2D texture;
+	public float threshold = 0;
+	// Creates a private material used to the effect
+	void Awake()
+	{
+		material = new Material(Shader.Find("Hidden/SwitchSceneUsingTexture"));
+		material.SetTexture("_ThresholdTex", texture);
+	}
+
+	// Postprocess the image
+	void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		material.SetFloat("_Threshold", threshold);
+		Graphics.Blit(source, destination, material);
+	}
+}
