5dac2d991bbe8f7dd6d96f81d245653ed035ee9f
jandy14 <seonguk14@changwon.ac.kr>
2020-06-04 02:14:12 +0900

    replacementshader 사용법 실습

---
+++ Assets/ReplacementShader/ShaderReplacement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderReplacement : MonoBehaviour
+{
+	public Shader replacementShader;
+	public string tagName;
+
+	// Start is called before the first frame update
+	private void OnEnable()
+	{
+		GetComponent<Camera>().SetReplacementShader(replacementShader, tagName);
+	}
+
+	private void OnDisable()
+	{
+		GetComponent<Camera>().ResetReplacementShader();
+	}
+}
