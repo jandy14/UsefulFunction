313c85a2378a949cf5a6a770ba69315faa710ba2
jandy14 <seonguk14@changwon.ac.kr>
2020-06-05 02:02:04 +0900

    RenderTexture와의 블렌딩을 이용한 아웃라인 제작중

---
+++ Assets/ReplacementShader/MixWithRenderTexture.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MixWithRenderTexture : MonoBehaviour
+{
+	private Material material;
+	// Creates a private material used to the effect
+	void Awake()
+	{
+		material = new Material(Shader.Find("Unlit/RenderTextureChecker"));
+	}
+
+	// Postprocess the image
+	void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		Graphics.Blit(source, destination, material);
+	}
+}
---
+++ Assets/ReplacementShader/RenderingAndProcessing.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderingAndProcessing : MonoBehaviour
+{
+	public float amount = 0.1f;
+	public Shader baseShader;
+
+	private static RenderTexture baseImg;
+	private static RenderTexture madeImg;
+
+	private Material mat;
+
+	private void OnEnable()
+	{
+		Camera camera = GetComponent<Camera>();
+		baseImg = new RenderTexture(Screen.width, Screen.height, 24);
+		madeImg = new RenderTexture(Screen.width, Screen.height, 0);
+		camera.targetTexture = baseImg;
+		camera.SetReplacementShader(baseShader, "");
+		Shader.SetGlobalTexture("_BaseImg", baseImg);
+		Shader.SetGlobalTexture("_MadeImg", madeImg);
+
+		mat = new Material(Shader.Find("Hidden/Blur"));
+		mat.SetFloat("_Amount", 0.005f);
+	}
+
+	private void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		Graphics.Blit(source, destination);
+
+		for (int i = 0; i < 4; ++i)
+		{
+			Graphics.Blit(source, madeImg, mat);
+			Graphics.Blit(madeImg, source, mat);
+		}
+		Graphics.Blit(source, madeImg, mat);
+	}
+}
