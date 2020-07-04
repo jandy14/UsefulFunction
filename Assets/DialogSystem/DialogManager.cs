df65bb1544df89db677a3d4635cad01ce32793d0
jandy14 <seonguk14@changwon.ac.kr>
2020-07-04 13:54:29 +0900

    한글자씩 띄우기 (다이얼로그 시스템 개발중 )

---
+++ Assets/DialogSystem/DialogManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogManager : MonoBehaviour
+{
+	public TMP_Text dialogText;
+
+    void Start()
+    {
+		StartCoroutine(Progress());
+    }
+
+
+	IEnumerator Progress()
+	{
+		dialogText.ForceMeshUpdate();
+		TMP_TextInfo textInfo = dialogText.textInfo;
+		int totalVisibleCharacters = textInfo.characterCount;
+		Debug.Log(textInfo.lineCount);
+		Debug.Log(textInfo.lineInfo[0].characterCount);
+		Debug.Log(textInfo.lineInfo[1].characterCount);
+		Debug.Log(textInfo.lineInfo[2].characterCount);
+		Debug.Log(totalVisibleCharacters);
+		int visibleCount = 0;
+
+		while(true)
+		{
+			dialogText.maxVisibleCharacters = visibleCount;
+			visibleCount += 1;
+
+			if (visibleCount > totalVisibleCharacters)
+				break;
+			yield return null;
+
+		}
+		yield return null;
+	}
+
+}
